726097df78f9c54ee8ae7d20b4a514db60a53dab
Matthew Roy <mroyucs@outlook.com>
2016-08-22 00:14:55 -0700

    Remove debug session variable

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/includes/php/init.php';
 
-$_SESSION['USER_LOGIN_STATE']=1;
 $isLoggedIn = check_login_state();
 if ($isLoggedIn === 0) {
 	header_redirect('index.php');
