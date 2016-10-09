fda2eb8fadeb968591d57eb5a73978dbb31b4e89
Matthew Roy <matthew.l.roy@gmail.com>
2016-10-08 17:58:01 -0700

    Change to Juxco headlines

--- includes/php/data-constants.php
+++ includes/php/data-constants.php
@@ -1,8 +1,8 @@
 <?php
-define('FULLY_QUALIFIED_URL', 'https://www.mive.io/');
+define('FULLY_QUALIFIED_URL', 'https://www.juxco.com/');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'mive_dba');
+define('DB_USER', 'juxco_dba');
 define('DB_PASS', 'dbpass');
 
 define('DB_USER_ACCOUNTS', 'user_accounts');
