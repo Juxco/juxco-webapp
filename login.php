c4460c5c5bb119b648e31714ec4eb3d65849234e
Matthew Roy <matthew.l.roy@gmail.com>
2016-12-03 14:31:09 -0800

    Added functionality for user to log in

--- login.php
+++ login.php
@@ -1,6 +1,33 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/php/init.php';
 
+if (check_form_post('loginSubmit') === 0) {
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_USER_ACCOUNTS);
+
+	if ($conn->connect_errno) {
+		die('couldnt connect to db');
+	}
+
+	$stmt = $conn->prepare('SELECT password, role_type FROM ' . TABLE_USER_ACCOUNTS . ' WHERE email=?');
+	$stmt->bind_param('s', $loginEmail);
+
+	$loginEmail = $_POST['loginEmail'];
+
+	$stmt->execute();
+	$stmt->bind_result($loginPassword, $roleType);
+	$stmt->fetch();
+
+	$stmt->close();
+	$conn->close();
+
+	if ($_POST['loginPassword'] === $loginPassword) {
+		invert_login_state();
+		$_SESSION['ROLE_TYPE'] = $roleType;
+	} else {
+		echo '<script type="text/javascript">alert(\'Invalid credentials.\');</script>';
+	}
+}
+
 if (check_login_state() === 0) {
 	header_redirect('index.php');
 }
--- register.php
+++ register.php
@@ -2,34 +2,24 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/php/init.php';
 
 if (check_form_post('registerSubmit') === 0) {
-	// create connection
 	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_USER_ACCOUNTS);
 
-	// check connection
 	if ($conn->connect_errno) {
 		die('couldnt connect to db');
 	}
 
-	// prepare sql statement
 	$stmt = $conn->prepare('INSERT INTO ' . TABLE_USER_ACCOUNTS  . '(email, username, password, role_type) VALUES (?, ?, ?, ?)');
 	$stmt->bind_param('ssss', $registerEmail, $registerUsername, $registerPassword, $registerRoleType);
 
-	// set params and  execute sql statement
 	$registerEmail = $_POST['registerEmail'];
 	$registerUsername = $_POST['registerUsername'];
 	$registerPassword = $_POST['registerPassword'];
 	$registerRoleType = $_POST['registerRoleType'];
 	$stmt->execute();
 
-	// check execution of sql statement
-
-	// close connection
 	$stmt->close();
 	$conn->close();
 
-	// check database for new record just added
-
-	// redirect
 	header_redirect('index.php');
 }
 
