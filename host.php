0c935866d17b6b571a79261c157b578a71f8594b
Matthew Roy <matthew.l.roy@gmail.com>
2016-12-07 20:44:46 -0800

    Finished host web app

--- host.php
+++ host.php
@@ -15,11 +15,42 @@ if ($conn->connect_errno) {
 	die('couldnt connect to db');
 }
 
+if (check_form_post('addPartySubmit') === 0) {
+	$stmt = $conn->prepare('INSERT INTO ' . TABLE_HOST_WAITLIST  . '(party_name, party_size, reservation) VALUES (?, ?, ?)');
+	$stmt->bind_param('sis', $addCustomerPartyName, $addCustomerPartySize, $addCustomerReservation);
+
+	$addCustomerPartyName = $_POST['addCustomerPartyName'];
+	$addCustomerPartySize = $_POST['addCustomerPartySize'];
+	$addCustomerReservation = $_POST['addCustomerReservation'];
+	$stmt->execute();
+
+	$stmt->close();
+}
+
 if (check_form_post('deleteEntry') === 0) {
 	$stmt = $conn->prepare('DELETE FROM ' . TABLE_HOST_WAITLIST . ' WHERE id=?');
-	$stmt->bind_param('s', $delEntryNum);
+	$stmt->bind_param('i', $delEntryNum);
 
 	$delEntryNum = $_POST['delEntryNum'];
+	$stmt->execute();
+
+	$stmt->close();
+}
+
+if (check_form_post('updateTableStatus') === 0) {
+	$stmt = $conn->prepare('UPDATE ' . TABLE_STATUS_TABLES . ' SET busboy_status=? WHERE id=?');
+	$stmt->bind_param('si',$newBusboyStatus , $tableNum);
+
+	$curBusboyStatus = $_POST['curBusboyStatus'];
+	if ($curBusboyStatus == "available") {
+		$newBusboyStatus = "occupied";
+	} else if ($curBusboyStatus == "occupied") {
+		$newBusboyStatus = "dirty";
+	} else if ($curBusboyStatus == "dirty") {
+		$newBusboyStatus = "available";
+	}
+	
+	$tableNum = $_POST['tableNum'];
 	$stmt->execute();
 
 	$stmt->close();
@@ -51,8 +82,56 @@ if ($result->num_rows > 0) {
     echo '<tr><td colspan="5">No results</td></tr>';
 }
 
-$conn->close();
 
 create_view('host2');
+
+$sql = 'SELECT id, seating_size, busboy_status, waiter_status FROM ' . TABLE_STATUS_TABLES;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+		echo '<tr>';
+		echo '<td>' . $row['id'] . '</td>';
+		echo '<td>' . $row['seating_size']  . '</td>';
+
+		if ($row['busboy_status'] == "available") {
+			echo '
+			<td class="success"">
+			  <form name="updateTableStatusForm" method="post">
+				<input type="hidden" value="' . $row['id'] . '" name="tableNum"></input>
+				<input type="hidden" value="' . $row['busboy_status'] . '" name="curBusboyStatus"></input>
+				<button id="updateTableStatus" name="updateTableStatus" type="submit" class="btn btn-success btn-block">' . $row['busboy_status']  . '</button>
+			  </form>
+			</td>';
+		} else if ($row['busboy_status'] == "occupied") {
+			echo '
+			<td class="danger">
+			  <form name="updateTableStatusForm" method="post">
+				<input type="hidden" value="' . $row['id'] . '" name="tableNum"></input>
+				<input type="hidden" value="' . $row['busboy_status'] . '" name="curBusboyStatus"></input>
+				<button id="updateTableStatus" name="updateTableStatus" type="submit" class="btn btn-danger btn-block">' . $row['busboy_status']  . '</button>
+			  </form>
+			</td>';
+		} else if ($row['busboy_status'] == "dirty") {
+			echo '
+			<td class="warning">
+			  <form name="updateTableStatusForm" method="post">
+				<input type="hidden" value="' . $row['id'] . '" name="tableNum"></input>
+				<input type="hidden" value="' . $row['busboy_status'] . '" name="curBusboyStatus"></input>
+				<button id="updateTableStatus" name="updateTableStatus" type="submit" class="btn btn-warning btn-block">' . $row['busboy_status']  . '</button>
+			  </form>
+			</td>';
+		}
+		
+		echo '<td>' . $row['waiter_status']  . '</td>';
+		echo '</tr>';
+    }
+} else {
+    echo '<tr><td colspan="4">No results</td></tr>';
+}
+
+$conn->close();
+
+create_view('host3');
 create_footer();
 ?>
